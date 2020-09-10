419ad7bdfffa40c8f0edb8e31320e524a0449dfe
Xiaolong Lin <xxl5453@psu.edu>
2020-09-10 20:19:37 +0000

    01

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include<readline/readline.h>
 #include <stdlib.h>
@@ -6,31 +7,31 @@ int main(void) {
   
   
   if  (grade>=93) {
-     printf("Your letter grade for CMPSC 131 is A");
+     printf("Your letter grade for CMPSC 131 is A.");
   }
   else if (grade>=90) {
-     printf("Your letter grade for CMPSC 131 is A-");
+     printf("Your letter grade for CMPSC 131 is A-.");
   }
   else if (grade>=87) {
-    printf("Your letter grade for CMPSC 131 is B+");
+    printf("Your letter grade for CMPSC 131 is B+.");
   }
   else if (grade>=83) {
-    printf("Your letter grade for CMPSC 131 is B");
+    printf("Your letter grade for CMPSC 131 is B.");
   }
   else if (grade>=80) {
-    printf("Your letter grade for CMPSC 131 is B-");
+    printf("Your letter grade for CMPSC 131 is B-.");
   }
   else if (grade>=77) {
-    printf("Your letter grade for CMPSC 131 is C+");
+    printf("Your letter grade for CMPSC 131 is C+.");
   }
   else if (grade>=70) {
-    printf("Your letter grade for CMPSC 131 is C");
+    printf("Your letter grade for CMPSC 131 is C.");
   }
   else if (grade>=60) {
-    printf("Your letter grade for CMPSC 131 is D");
+    printf("Your letter grade for CMPSC 131 is D.");
   }  
   else   {
-    printf("Your letter grade for CMPSC 131 is F");
+    printf("Your letter grade for CMPSC 131 is F.");
   }  
     
   
