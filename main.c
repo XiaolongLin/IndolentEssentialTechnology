55f142ccd1878d01d87831b8792840be45f5c9d7
Xiaolong Lin <xxl5453@psu.edu>
2020-09-18 03:42:03 +0000

    05

--- main.c
+++ main.c
@@ -2,45 +2,27 @@
 #include <stdio.h>
 #include<readline/readline.h>
 #include <stdlib.h>
-int main(void) {
-  double grade = atof(readline("Enter your CMPSC 131 grade: "));
-  
-  
-  if  (grade>=93) {
-     printf("Your letter grade for CMPSC 131 is A.\n");
-  }
-  else if (grade>=90) {
-     printf("Your letter grade for CMPSC 131 is A-.\n");
-  }
-  else if (grade>=87) {
-    printf("Your letter grade for CMPSC 131 is B+.\n");
-  }
-  else if (grade>=83) {
-    printf("Your letter grade for CMPSC 131 is B.\n");
-  }
-  else if (grade>=80) {
-    printf("Your letter grade for CMPSC 131 is B-.\n");
-  }
-  else if (grade>=77) {
-    printf("Your letter grade for CMPSC 131 is C+.\n");
-  }
-  else if (grade>=70) {
-    printf("Your letter grade for CMPSC 131 is C.\n");
-  }
-  else if (grade>=60) {
-    printf("Your letter grade for CMPSC 131 is D.\n");
-  }  
-  else   {
-    printf("Your letter grade for CMPSC 131 is F.\n");
-  }  
-    
-  
-    
 
-  
-  
-  
 
-  
+int sum_n(int n){
+  if (n == 0){
+    return 0;
+  }
+    else{
+      return n + sum_n(n-1);
+    }
+}
+void print_n(const char *s, int n){
+  if (n != 0){
+    printf("%s\n",s);
+    print_n(s, n-1);
+  }
+}
+
+int main(void){
+  int result = atoi(readline("Enter an int: "));
+  printf("sum is %d.\n",sum_n(result));
+  char *string = readline("Enter a string: ");
+  print_n(string,result);
   return 0;
 }
